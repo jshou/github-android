26cb985f07f04f1f37906850c125f9c284f138f1
jshou <joshuahou@gmail.com>
2012-03-30 01:13:40 -0700

    logout button, doesn't clear activity stack though

--- github-android/src/main/java/com/joshuahou/githubandroid/activities/Main.java
+++ github-android/src/main/java/com/joshuahou/githubandroid/activities/Main.java
@@ -2,6 +2,7 @@ package com.joshuahou.githubandroid.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -20,5 +21,13 @@ public class Main extends Activity {
 
     public void lookAtRepos(View v) {
         startActivity(new Intent(this, Repositories.class));
+    }
+    
+    public void logout(View v) {
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.remove("token");
+        editor.commit();
+
+        startActivity(new Intent(this, Signin.class));
     }
 }
