4f7832b2cd73bcb45f562487618f23c94f45119a
jshou <joshuahou@gmail.com>
2012-04-02 09:10:22 -0700

    more partial impl of PullRequest, started some testing of data package

---
+++ github-android-it/src/main/java/com/joshuahou/githubandroid/test/data/RepositoryTest.java
@@ -0,0 +1,13 @@
+package com.joshuahou.githubandroid.test.data;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(RobolectricTestRunner.class)
+public class RepositoryTest {
+    @Test
+    public void repositoryTests() {
+
+    }
+}
--- github-android/src/main/java/com/joshuahou/githubandroid/data/PullRequest.java
+++ github-android/src/main/java/com/joshuahou/githubandroid/data/PullRequest.java
@@ -8,21 +8,26 @@ import com.google.gson.JsonObject;
 import java.util.List;
 
 public class PullRequest {
+
     private String title;
     private String body;
     private String assigner;
     private String assignee;
-    private String diff;
+    private String diffUrl;
     private String repo;
 
     public PullRequest(JsonObject json) {
-//        this.name = json.get("name").getAsString();
-//        this.private_repo = json.get("private").getAsBoolean();
-//        this.owner = json.get("owner").getAsJsonObject().get("login").getAsString();
+        this.title = json.get("title").getAsString();
+        this.body = json.get("body").getAsString();
+        this.assigner = json.get("user").getAsJsonObject().get("login").getAsString();
+        this.assignee = json.get("assignee").getAsJsonObject().get("login").getAsString();
+
+        this.diffUrl = json.get("pull_request").getAsJsonObject().get("diff_url").getAsString();
+        this.repo = diffUrl.replaceAll("/([A-Za-z0-9]+)/issues", "$1");
     }
 
-    public static List<PullRequest> createRepositories(JsonElement json){
-        JsonArray jsonArray= json.getAsJsonArray();
+    public static List<PullRequest> createPullRequests(JsonElement json) {
+        JsonArray jsonArray = json.getAsJsonArray();
 
         List<PullRequest> pullRequests = Lists.newArrayList();
         for (JsonElement e : jsonArray) {
@@ -30,5 +35,29 @@ public class PullRequest {
         }
 
         return pullRequests;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getAssigner() {
+        return assigner;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public String getDiffUrl() {
+        return diffUrl;
+    }
+
+    public String getRepo() {
+        return repo;
     }
 }
